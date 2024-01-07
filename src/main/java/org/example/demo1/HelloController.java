1b4190d616075f855bde8dc4e79e6926d3ebf586
Pavel Davydov <Davydov.works@gmail.com>
2024-01-07 10:09:26 -0500

    Add firstLine

--- src/main/java/org/example/demo1/HelloController.java
+++ src/main/java/org/example/demo1/HelloController.java
@@ -26,11 +26,11 @@ public class HelloController {
         int rowIndex = GridPane.getRowIndex(btn) == null ? 0 : GridPane.getRowIndex(btn);
         int columIndex = GridPane.getColumnIndex(btn) == null ? 0 : GridPane.getColumnIndex(btn);
 
-      gameField [rowIndex] [columIndex] = nowSym;
+        gameField[rowIndex][columIndex] = nowSym;
 
         btn.setText(String.valueOf(nowSym));
 
-        if (gameField [0] [0] == gameField && gameField [0] [0] == gameField [0] [2]) {
+        if (gameField[0][0] == gameField [0][1] && gameField[0][0] == gameField[0][2] && !Character.isWhitespace(gameField[0][0]))  {
             isGame = false;
         }
         nowSym = nowSym == 'x' ? 'o' : 'x';
