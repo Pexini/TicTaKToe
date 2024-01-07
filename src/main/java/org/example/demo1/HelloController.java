b20938f31ea35f91656eaa3098d48041bd9b524c
Pavel Davydov <Davydov.works@gmail.com>
2024-01-07 10:54:35 -0500

    Add win

--- src/main/java/org/example/demo1/HelloController.java
+++ src/main/java/org/example/demo1/HelloController.java
@@ -35,33 +35,49 @@ public class HelloController {
         btn.setText(String.valueOf(nowSym));
 
         if (gameField[0][0] == gameField[0][1] && gameField[0][0] == gameField[0][2] && (gameField[0][0] == 'x' || gameField[0][0] == 'o')) {
-           Alert alert = new Alert(Alert.AlertType.INFORMATION, "Победитель :" + btn.getText() + " # ", ButtonType.OK);
+           Alert alert = new Alert(Alert.AlertType.INFORMATION, "Победитель :" + btn.getText() + " Ура ", ButtonType.OK);
             alert.showAndWait();
             isGame = false;
         } else if (gameField[1][0] == gameField[1][1] && gameField[1][0] == gameField[1][2] && (gameField[1][0] == 'x' || gameField[1][0] == 'o')) {
+            Alert alert = new Alert(Alert.AlertType.INFORMATION, "Победитель :" + btn.getText() + " Ура ", ButtonType.OK);
+            alert.showAndWait();
             isGame = false;
         }
         if (gameField[2][0] == gameField[2][1] && gameField[2][0] == gameField[2][2] && (gameField[2][0] == 'x' || gameField[2][0] == 'o')) {
+            Alert alert = new Alert(Alert.AlertType.INFORMATION, "Победитель :" + btn.getText() + " Ура ", ButtonType.OK);
+            alert.showAndWait();
             isGame = false;
         }
         if (gameField[0][0] == gameField[1][0] && gameField[0][0] == gameField[2][0] && (gameField[0][0] == 'x' || gameField[0][0] == 'o')) {
+            Alert alert = new Alert(Alert.AlertType.INFORMATION, "Победитель :" + btn.getText() + " Ура ", ButtonType.OK);
+            alert.showAndWait();
             isGame = false;
         }
         if (gameField[0][1] == gameField[1][1] && gameField[0][1] == gameField[2][1] && (gameField[0][1] == 'x' || gameField[0][0] == 'o')) {
+            Alert alert = new Alert(Alert.AlertType.INFORMATION, "Победитель :" + btn.getText() + " Ура ", ButtonType.OK);
+            alert.showAndWait();
             isGame = false;
         }
 
         if (gameField[0][2] == gameField[1][2] && gameField[0][2] == gameField[2][2] && (gameField[0][2] == 'x' || gameField[0][0] == 'o')) {
+            Alert alert = new Alert(Alert.AlertType.INFORMATION, "Победитель :" + btn.getText() + " Ура ", ButtonType.OK);
+            alert.showAndWait();
             isGame = false;
         }
 
         if (gameField[0][0] == gameField[1][1] && gameField[0][0] == gameField[2][2] && (gameField[0][0] == 'x' || gameField[0][0] == 'o')) {
+            Alert alert = new Alert(Alert.AlertType.INFORMATION, "Победитель :" + btn.getText() + " Ура ", ButtonType.OK);
+            alert.showAndWait();
             isGame = false;
         }
         if (gameField[2][2] == gameField[1][1] && gameField[2][2] == gameField[0][2] && (gameField[2][2] == 'x' || gameField[0][0] == 'o')) {
+            Alert alert = new Alert(Alert.AlertType.INFORMATION, "Победитель : Это " + btn.getText() + " Ура ", ButtonType.OK);
+            alert.showAndWait();
             isGame = false;
         }
         if (gameField[2][0] == gameField[1][1] && gameField[2][0] == gameField[0][2] && (gameField[2][0] == 'x' || gameField[0][0] == 'o')) {
+            Alert alert = new Alert(Alert.AlertType.INFORMATION, "Победитель :" + btn.getText() + " Ура ", ButtonType.OK);
+            alert.showAndWait();
             isGame = false;
         }
 
